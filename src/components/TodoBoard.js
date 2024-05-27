2eebb7ff1ad326441d1993a09a23c781c082427f
suhyang1166 <yooyootea1166@gmail.com>
2024-05-27 20:24:47 +0900

    로그인 완료

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -2,24 +2,20 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 const TodoBoard = ({ todoList, deleteItem, toggleComplete }) => {
-    return (
-        <div>
-            <h2>Todo List</h2>
-            {todoList.length > 0 ? (
-                todoList.map((item, idx) => (
-                    <TodoItem
-                        item={item}
-                        key={idx}
-                        deleteItem={deleteItem}
-                        toggleComplete={toggleComplete}
-                    />
-                ))
-            ) : (
-                <h2>There is no Item to show</h2>
-            )}
-            {/* <TodoItem/> will be here once we get the todoList */}
-        </div>
-    );
+  return (
+    <div>
+      <h2>Todo List</h2>
+      {todoList.length > 0 &&
+        todoList.map((item, index) => (
+          <TodoItem
+            item={item}
+            key={index}
+            deleteItem={deleteItem}
+            toggleComplete={toggleComplete}
+          />
+        ))}
+    </div>
+  );
 };
 
 export default TodoBoard;
